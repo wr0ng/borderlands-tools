9f3eff47049f664be41685bddc720d7657921237
Michael Floering <michaelfloering@gmail.com>
2012-11-04 17:22:35 -0500

    Add stub for setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# TODO
+
+
+#1) chmod all scripts in this repo
+#2) do a check that sees which of the requirements and extras the user has installed and throws errors or warnings for missing ones, respectively. exit on error (i.e. if missing JACK).
+#X) make borderlands.desktop somewhere
+#  a) replace Exec path with one that uses the actual users /home/username/ piece
+#  b) TODO
