07829d233312405a59fc854ecc43ed2a8d6510b9
Michael Floering <michaelfloering@gmail.com>
2012-11-04 17:23:51 -0500

    Add note about sed

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@
 #1) chmod all scripts in this repo
 #2) do a check that sees which of the requirements and extras the user has installed and throws errors or warnings for missing ones, respectively. exit on error (i.e. if missing JACK).
 #X) make borderlands.desktop somewhere
-#  a) replace Exec path with one that uses the actual users /home/username/ piece
+#  a) replace Exec path with one that uses the actual users /home/username/ piece -- http://www.brunolinux.com/02-The_Terminal/Find_and%20Replace_with_Sed.html
 #  b) TODO
