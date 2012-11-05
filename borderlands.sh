a0114528728838792f0bfec4b45da27214fecb97
Michael Floering <michaelfloering@gmail.com>
2012-11-04 21:18:35 -0500

    Consolidate borderlands_with_recording.sh and borderlands.sh, and revise

--- borderlands.sh
+++ borderlands.sh
@@ -7,13 +7,16 @@
 # 2) start Borderlands
 
 source $(dirname $0)/colorize.sh
- 
-echo "==> Starting JACK" | colorize green
-bash start_jack.sh
 
-echo "==> Sleeping for 2 seconds while QJackCtl starts." | colorize yellow
-echo "==> (Borderlands will silently die if JACK has finished initializing. If you hit an issue where Borderlands is not starting from this script, make this sleep longer.)" | colorize yellow
-sleep 2s
+echo "==> Starting JACK" | colorize yellow
+bash start_jack.sh --preset=${jack_preset}
+
+## Sleep
+if [ -z "${sleep_seconds}" ]; then
+    sleep_seconds=3
+fi
+echo "... Sleeping for ${sleep_seconds} seconds while QJackCtl starts." | colorize yellow
+sleep ${sleep_seconds}s
 
 echo "==> Changing into Borderlands directory" | colorize yellow
 # Wildcard used so it is not version-contingent, but this will break
@@ -22,6 +25,17 @@ cd ~/Borderlands*/Borderlands/
 
 echo "==> Starting Borderlands" | colorize yellow
 ./Borderlands &
+status=$?
+if test $status -eq 0 #successful
+then
+    echo "... Successfully started Borderlands" | colorize green
+else
+    echo "... Failed to start Borderlands" | colorize red
+    echo "... (Borderlands dies if JACK has not finished initializing. If you hit an issue where Borderlands is not starting from this script, make it sleep for longer.)" | colorize red
+fi
 
 echo "==> Returning to previous directory" | colorize yellow
 cd -
+
+echo "==> Start recording" | colorize yellow
+bash record.sh
