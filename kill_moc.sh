d91d91bd18c63cc44cc6dbb0fc74cd033b6b6fc8
Michael Floering <michaelfloering@gmail.com>
2012-11-09 18:26:20 -0500

    Remove moc(p) -related stuff, since we're using jack.play now

--- kill_moc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# kill_moc.sh: kill all processes with moc in the title
-
-source $(dirname $0)/colorize.sh # echo hi | colorize yellow
-
-echo "==> Killing all processes named moc" | colorize yellow
-kill -9 $(ps -A | grep [m]oc | awk '{print $1}')
