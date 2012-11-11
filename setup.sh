7a4bfba8401a9ede11ec398fd6681a81b2facefa
Michael Floering <michaelfloering@gmail.com>
2012-11-10 21:05:25 -0500

    Accidentally make everything executable ... woops

--- setup.sh
+++ setup.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
-# template.sh: template for shell scripts
+# setup.sh: template for shell scripts
 # author: Michael Floering
-# 
-# What it does:
-# 1) Act as an example
-#
-# Example usage:
-# Just duplicate it to stub out new scripts, like:
-# cp template.sh new_shell_script_name.sh
 
 source $(dirname $0)/colorize.sh
 source $(dirname $0)/launch_if_possible.sh
  
-echo "==> I am green in this area" | colorize green
-# now run the command described above
-launch foo
+echo "==> Replacing all occurences of \$HOME in .desktop files with $HOME" | colorize yellow
+
+function render_with_real_home {
+
+# Example: given arg 'Borderlands.desktop' it will read in 'Borderlands.desktop.template', replace all instances of string $HOME with the actual $HOME var, then output to Borderlands.desktop
+
+    _home="$HOME"
+    _home="${_home//\//\\/}"
+
+    cat $1.template | sed -e "s/\$HOME/$_home/g" > $1
+
+} # render_with_real_home
+
+# do that for both .desktop files
+render_with_real_home Borderlands.desktop
+render_with_real_home BorderlandsLoopManager.desktop
+
+echo "==> Making everything executable" | colorize yellow
+
+# chmod everything all .sh files and .desktop files in directory
+chmod 755 *.sh
+chmod 755 *.desktop
+
+echo "==> Moving .desktop files to ~/.local/share/applications" | colorize yellow
+
+# move the .desktop files to the magical place
+cp BorderlandsLauncher.desktop ~/.local/share/applications/BorderlandsLauncher.desktop
+cp BorderlandsLoopsManager.desktop ~/.local/share/applications/BorderlandsLoopsManager.desktop
+
+# open folder there
+nautilus ~/.local/share/applications
+
+echo "!!! Now, drag BorderlandsLauncher and BorderlandsLoopsManager onto your launcher panel. Then right click each icon and click 'Keep in Launcher' if you want to keep them there. Press any key to finish." | colorize red
